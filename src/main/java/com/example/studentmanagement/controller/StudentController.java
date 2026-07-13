afb617d7dfc36cf4f034a0c518fa8e1746396775
Susobhan Behera <susobhanbehera211@gmail.com>
2026-07-13 10:53:09 +0530

    refactor: implement consisten API response structure

--- src/main/java/com/example/studentmanagement/controller/StudentController.java
+++ src/main/java/com/example/studentmanagement/controller/StudentController.java
@@ -2,9 +2,9 @@ package com.example.studentmanagement.controller;
 
 import com.example.studentmanagement.dto.StudentRequestDto;
 import com.example.studentmanagement.dto.StudentResponseDto;
-import com.example.studentmanagement.entity.Student;
 import com.example.studentmanagement.service.StudentService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,32 +16,37 @@ import java.util.List;
 public class StudentController {
     private final StudentService studentService;
 
-
     public StudentController(StudentService studentService){
         this.studentService=studentService;
     }
 
-    /*@PostMapping
-    public Student createStudent(@RequestBody Student student){ return studentService.saveStudent(student); }*/
-
-    /*@PostMapping
-    public Student createStudent(@Valid @RequestBody StudentRequestDto dto) {return studentService.saveStudent(dto);}*/
-
     @PostMapping
-    public StudentResponseDto createStudent(@Valid @RequestBody StudentRequestDto dto) {return studentService.saveStudent(dto);}
+    public ResponseEntity<StudentResponseDto> createStudent(@Valid @RequestBody StudentRequestDto dto) {
+        StudentResponseDto responseDto= studentService.saveStudent(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
 
     @GetMapping("/{id}")
-    public StudentResponseDto getStudentById(@PathVariable Long id) { return studentService.getStudentById(id);}
+    public ResponseEntity<StudentResponseDto> getStudentById(@PathVariable Long id) {
+        StudentResponseDto responseDto = studentService.getStudentById(id);
+        return ResponseEntity.ok(responseDto);
+    }
 
     @GetMapping
-    public List<StudentResponseDto> getAllStudents() {
-        return studentService.getAllStudents();
+    public ResponseEntity<List<StudentResponseDto>> getAllStudents() {
+
+        List<StudentResponseDto> responseDtos= studentService.getAllStudents();
+
+        return ResponseEntity.ok(responseDtos);
     }
 
     @PutMapping("/{id}")
-    public StudentResponseDto updateStudent(@PathVariable  Long id  ,
+    public ResponseEntity<StudentResponseDto> updateStudent(@PathVariable  Long id  ,
                                             @Valid @RequestBody StudentRequestDto dto) {
-        return studentService.updateStudent(id,dto);
+
+        StudentResponseDto responseDto= studentService.updateStudent(id,dto);
+
+        return ResponseEntity.ok(responseDto);
     }
 
     @DeleteMapping("/{id}")
